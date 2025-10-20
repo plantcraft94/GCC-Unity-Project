19d058c05547477cc03531e8a36519325202ba85
plantcraft94 <plantcraft60@gmail.com>
2025-10-20 23:59:33 +0700

    ??

--- Assets/GAME/Scripts/InteractionRange.cs
+++ Assets/GAME/Scripts/InteractionRange.cs
@@ -5,12 +5,10 @@ using UnityEngine;
 public class InteractionRange : MonoBehaviour
 {
 	NPC nPC;
-	PlayerAttack pa;
 
 	private void Awake()
 	{
 		nPC = transform.parent.gameObject.GetComponent<NPC>();
-		pa = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerAttack>();
 	}
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
